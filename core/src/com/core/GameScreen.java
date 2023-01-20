762d249a6fb23658c4851ccd642f1e1e521ee72e
Lee Rogers <lrogersgm@gmail.com>
2023-01-20 14:19:58 +0000

    Pencilled in render and update methods for the two screens just to show where they should go (not in Boot), but can be modified and we know what we need to do.

--- core/src/com/core/GameScreen.java
+++ core/src/com/core/GameScreen.java
@@ -1,19 +1,50 @@
 package com.core;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class GameScreen extends ScreenAdapter {
 
     private OrthographicCamera camera;
     private Box2DDebugRenderer box2DDebugRenderer;
+    private SpriteBatch batch;
+    private World world;
 
     public GameScreen(OrthographicCamera camera) {
         this.camera = camera;
         this.camera.position.set(new Vector3(Boot.INSTANCE.getScreenWidth() / 2, Boot.INSTANCE.getScreenHeight() / 2, 0));
         this.box2DDebugRenderer = new Box2DDebugRenderer();
+        this.batch = new SpriteBatch();
+        this.world = new World(new Vector2(0, 0), false);
     }
 
+    public void update() {
+        world.step(1 / 60f, 6, 2);
+
+        this.camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
+    }
+
+    @Override
+    public void render(float delta) {
+        update();
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+
+        batch.end();
+
+        // For debugging purposes:
+        //this.box2DDebugRenderer.render(world, camera.combined.scl(Const.PPM));
+    }
 }
