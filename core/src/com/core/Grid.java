38f8c96c7fe98580c17305a8adf8b6360e2df040
elliott62217 <erobinsonfarrelly@gmail.com>
2023-02-01 20:48:02 +0000

    added asset hopefully

--- core/src/com/core/Grid.java
+++ core/src/com/core/Grid.java
@@ -34,6 +34,7 @@ public class Grid extends ScreenAdapter {
         }
     }
 
+
     public void render () {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
