9b9f293919253e8185052f5d675fbf6ce404e7a5
Lee Rogers <lrogersgm@gmail.com>
2023-02-04 15:02:00 +0000

    Additional API features and planning. Some commentary in comments on approach

--- core/src/com/core/map/Tile.java
+++ core/src/com/core/map/Tile.java
@@ -8,6 +8,10 @@ public class Tile extends Rectangle
     //value between 0 and 1, google says a rig can produce as many as 5000 barrels a day, so multiply by this(just an idea)
     //supposedly some countries produce 100s of thousands per rig per day(middle east), so maybe a country modifier could be useful
     //potentially increment decay of this value also? for if oil runs out, and also natural fluctuation
+
+    // (^) nice research, but I worry if we try to model the real world like that we're going to hit problems with max integers very quickly
+    // I only saw this after writing the Location, Resource, Extraction etc. outlines, but I think keeping it a little more arcade(y) is the way to go, at least at first.
+
     private int tileType;
     //0 for water
     //1 for land
@@ -50,5 +54,9 @@ public class Tile extends Rectangle
     public int getType()
     {
         return tileType;
+    }
+
+    public Location getLocation() {
+        return location;
     }
 }
