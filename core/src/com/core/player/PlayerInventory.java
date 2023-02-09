35401438673dbb91fad1182a03be6c14651a601e
HannahWhitham <whitham.hannah@gmail.com>
2023-02-09 13:55:19 +0000

    Created a method to upadate the player's funds and income when an extractor is upgraded. Also created a method that adds the current income into the player's funds

--- core/src/com/core/player/PlayerInventory.java
+++ core/src/com/core/player/PlayerInventory.java
@@ -19,13 +19,39 @@ public class PlayerInventory {
     }
 
     /**
-     * Adds an extractor to the player's inventory
+     * Updates the inventory when an extractor is upgraded
+     * @param extractorID what extractor is being upgraded
+     */
+    public void upgradeExtractor(int extractorID){
+
+        //gets the extractor that is being upgraded
+        Extractor extractorToUpdate = this.getExtractor(extractorID);
+
+        //Player pays for the upgrade and income is updated with the new income
+        this.funds = this.funds - extractorToUpgrade.getUpdateCost();
+
+        //this needs to be the difference in income(since you are upgrading instead of buying a new one)
+        this.income = this.income + extractorToUpdate.getIncomeDiff();
+
+
+    }
+
+    /**
+     * Adds the income to the player's funds
+     */
+    public void updateFunds(){
+        this.funds = this.funds + this.income;
+    }
+
+    /**
+     * Adds an extractor to the player's inventory, takes price out of funds and adds income
      * @param extractor The extractor to be added to the player's inventory
      */
     public void addExtractor(Extractor extractor){
 
         this.extractors.add(extractor);
-
+        this.funds = this.funds - extractor.getPrice();//updates funds
+        this.income = this.income + extractor.getIncome();//updates income
     }
 
     /**
@@ -50,6 +76,8 @@ public class PlayerInventory {
         catch (Exception e){
             System.out.println("Unable to find extractor with that ID");
         }
+
+        return null;
 
     }
 
