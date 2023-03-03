722990573d944a0711d06ed6586c2d280ca6c911
Lee Rogers <lrogersgm@gmail.com>
2023-03-03 04:42:27 +0000

    Fully implemented drop down extraction selection

--- core/src/com/core/Const.java
+++ core/src/com/core/Const.java
@@ -1,5 +1,7 @@
 package com.core;
 
+import java.util.*;
+
 public class Const{
 
     //Pixel per metre
@@ -15,4 +17,50 @@ public class Const{
     //Base climate health
     public static final Double baseHealth = 1000.0;
 
+    // Extractor paths
+    public static final Map<String, ArrayList<String>> paths;
+    static {
+        Map<String, ArrayList<String>> initMap = new HashMap<String, ArrayList<String>>();
+
+        // Coal
+        ArrayList<String> coalPaths = new ArrayList<String>();
+        coalPaths.add("land-coal-mine.png");
+        coalPaths.add("sea-coal-mine.png");
+        initMap.put(Const.coal, coalPaths);
+        // Gas
+        ArrayList<String> gasPaths = new ArrayList<String>();
+        gasPaths.add("land-gas.png");
+        gasPaths.add("sea-gas.png");
+        initMap.put(Const.gas, gasPaths);
+        // Nuclear
+        ArrayList<String> nuclearPaths = new ArrayList<String>();
+        nuclearPaths.add("nuclear.png");
+        initMap.put(Const.nuclear, nuclearPaths);
+        // Oil
+        ArrayList<String> oilPaths = new ArrayList<String>();
+        oilPaths.add("land-rig.png");
+        oilPaths.add("sea-rig.png");
+        initMap.put(Const.oil, oilPaths);
+        // Solar
+        ArrayList<String> solarPaths = new ArrayList<String>();
+        solarPaths.add("solar-land.png");
+        initMap.put(Const.solar, solarPaths);
+        // Wind
+        ArrayList<String> windPaths = new ArrayList<String>();
+        windPaths.add("wind-turbine-land.png");
+        windPaths.add("wind-turbine-sea.png");
+        initMap.put(Const.wind, windPaths);
+        // Hydro
+        ArrayList<String> hydroPaths = new ArrayList<String>();
+        hydroPaths.add("hydro-land.png");
+        hydroPaths.add("hydro-sea.png");
+        initMap.put(Const.hydro, hydroPaths);
+        // Geothermal
+        ArrayList<String> geothermalPaths = new ArrayList<String>();
+        geothermalPaths.add("geothermal-land.png");
+        geothermalPaths.add("geothermal-sea.png");
+        initMap.put(Const.geothermal, geothermalPaths);
+
+        paths = Collections.unmodifiableMap(initMap);
+    }
 }
