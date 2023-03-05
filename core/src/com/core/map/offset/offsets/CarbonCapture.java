4f64c49e52d4021ef408a4d0ce85da9f7fb4c253
Harry Cheung <h.m.h.cheung@lancaster.ac.uk>
2023-03-05 23:11:31 +0000

    Added carbon capture class

--- core/src/com/core/map/offset/offsets/CarbonCapture.java
+++ core/src/com/core/map/offset/offsets/CarbonCapture.java
@@ -6,4 +6,21 @@ import java.util.Random;
 
 public class CarbonCapture extends Offset {
     private Random rand = new Random();
+    private double effectLowerBound, effectUpperBound;
+    private double costLowerBound, costUpperBound;
+    private double maintenanceLowerBound, maintenanceUpperBound;
+
+    public CarbonCapture() {
+        this.effectLowerBound = 10;
+        this.effectUpperBound = 50;
+        setEffect((effectLowerBound + (effectUpperBound - effectLowerBound * rand.nextDouble())));
+
+        this.costLowerBound = 50;
+        this.costUpperBound = 100;
+        setCost((costLowerBound + (costUpperBound - costLowerBound * rand.nextDouble())));
+
+        this.maintenanceLowerBound = 5;
+        this.maintenanceUpperBound = 10;
+        setMaintenance(maintenanceLowerBound + (maintenanceUpperBound = maintenanceUpperBound * rand.nextDouble()));
+    }
 }
